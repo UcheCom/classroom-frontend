2c5436c98da9fd695bb3a43437a43858020b46dc
Uchenna Oko <uche4newton1981@gmail.com>
2026-02-07 00:59:54 +0100

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const DEPARTMENTS = [
+    'computer_science',
+    'mathematics',
+    'physics',
+    'chemistry',
+    'biology',
+];
+
+export const DEPARTMENTS_OPTIONS = DEPARTMENTS.map(dept => ({
+    value: dept,
+    label: dept,
+}));
